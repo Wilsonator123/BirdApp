acccd3bf60dfb965f3e4b4cdac1039cd4504edb9
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-11-24 17:22:28 +0000

    Added Header + Footer and React Navigator

--- screens/home.jsx
+++ screens/home.jsx
@@ -3,42 +3,54 @@ import {
   SafeAreaView,
   StyleSheet,
   View,
+  TouchableOpacity,
+Text,
 } from 'react-native';
-import MapView from 'react-native-maps';
-
+import MapView, {Marker, Callout} from 'react-native-maps';
+import { useNavigation } from '@react-navigation/native';
+const pointer = require('../assets/pointer1-0.png');
 const base = require('../colors')
 
 function App(): JSX.Element {
     const [coordinates, setCoordinates] = React.useState([1.236603,52.635453]);
-    const onDrag = (e) => {
-        console.log(e.geometry.coordinates);
-        setCoordinates(e.geometry.coordinates);
-    }
-
+    const [showCallout, setShowCallout] = React.useState(false);
+    const navigator = useNavigation();
 
   return (
       <View style={base.body}>
             <View style={base.main}>
-            <MapView
-            style={styles.map}
-              initialRegion={{
-                latitude: 52.635453,
-                longitude: 1.236603,
-                latitudeDelta: 0.01,
-                longitudeDelta: 0.005,
-              }}
-            />
+                <MapView
+                style={styles.map}
+                  initialRegion={{
+                    latitude: coordinates[1],
+                    longitude: coordinates[0],
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.005,
+                  }}
+                >
+                   <Marker
+                        coordinate={{latitude: coordinates[1], longitude: coordinates[0]}}
+                        draggable
+                        onDragEnd={() => setShowCallout(true)}
+                        onDragStart={() => setShowCallout(false)}
+                        image={pointer}
+                     >
+                     {showCallout && <CustomCallout />}
+                    </Marker>
+                </MapView>
 
             </View>
-  </View>
+    </View>
   );
 }
 
-const CountButton = () => {
+const CustomCallout = () => {
     return (
-        <View>
-            <Text style={base.buttonText}>Start count</Text>
-        </View>
+        <Callout onPress={() => navigator.navigate("Count")}>
+            <View>
+                <Text>Custom Callout</Text>
+            </View>
+        </Callout>
     )
 }
 
@@ -47,7 +59,6 @@ const styles = StyleSheet.create({
         flex: 1,
         margin: 5,
     },
-
 })
 
 
